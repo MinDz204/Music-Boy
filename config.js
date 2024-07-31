0ec4d4c781f89a170e7bd54c3cba61a1650bdb98
MinDz204 <160019862+MinDz204@users.noreply.github.com>
2024-07-31 11:45:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -23,14 +23,14 @@
 
 module.exports = {
   TOKEN: "",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["1004206704994566164", "1229076807379194041"], 
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   musicardTheme:"themes16", //Goes from themes1 to themes19
-  activityName: "You", // This is bot status Write Anything here 
+  activityName: "YuongzMin", // This is bot status Write Anything here 
   activityType: "WATCHING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "https://discord.gg/mVUMG7tdRa",
   CheckmarkIcon: "https://cdn.discordapp.com/attachments/1230824451990622299/1230836684774576168/7762-verified-blue.gif",
   MusicIcon:"https://media.discordapp.net/attachments/1230824451990622299/1236664581364125787/music-play.gif",
   embedTimeout: 5,  // Timeout before the button interaction embeds are deleted ( Default - 5 seconds)
